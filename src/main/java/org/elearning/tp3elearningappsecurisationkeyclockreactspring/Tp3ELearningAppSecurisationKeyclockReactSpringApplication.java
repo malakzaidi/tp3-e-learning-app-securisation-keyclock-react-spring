840b4db5092e543773eba568c34f41c8c6b922ec
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-12-11 00:57:47 +0100

    add the project to github

---
+++ src/main/java/org/elearning/tp3elearningappsecurisationkeyclockreactspring/Tp3ELearningAppSecurisationKeyclockReactSpringApplication.java
@@ -0,0 +1,13 @@
+package org.elearning.tp3elearningappsecurisationkeyclockreactspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3ELearningAppSecurisationKeyclockReactSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3ELearningAppSecurisationKeyclockReactSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/elearning/tp3elearningappsecurisationkeyclockreactspring/Tp3ELearningAppSecurisationKeyclockReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elearning.tp3elearningappsecurisationkeyclockreactspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3ELearningAppSecurisationKeyclockReactSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
